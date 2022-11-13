513c2509da69fa4b2bebb82f22a8a6102da2107c
LuisF <luis-lipe@hotmail.com>
2022-11-13 13:44:05 -0300

    Context created.

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,22 @@ import { Home } from "./pages/Home/Home";
 import { Store } from "./pages/Store/Store";
 import { About } from "./pages/About/About";
 import { Navbar } from "./components/Navbar/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 
 function App() {
 
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <Container className="mb-4">
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
-    </>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </ShoppingCartProvider>
     
   )
 }
