689ae2c2215955d3d174a57c885e27df998fe589
LuisF <luis-lipe@hotmail.com>
2022-11-13 13:53:31 -0300

    Added functions to storeItem

--- src/components/StoreItem/StoreItem.tsx
+++ src/components/StoreItem/StoreItem.tsx
@@ -1,10 +1,13 @@
 import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
 import { IStoreItem } from "../../Interfaces/IStoreItem";
 import { formatCurrency } from "../../utils/formatCurrency";
 
 export function StoreItem({ id, name, price, imgUrl }: IStoreItem) {
 
-  let quantity = 0;
+  const { getItemQuantity, increaseItemQuantity, decreaseItemQuantity, removeFromCart } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
 
   return (
     <Card className="h-100">
@@ -17,18 +20,18 @@ export function StoreItem({ id, name, price, imgUrl }: IStoreItem) {
 
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100">+ Add To Cart</Button>
+            <Button className="w-100" onClick={() => increaseItemQuantity(id)}>+ Add To Cart</Button>
           ) : 
             <div className="d-flex align-items-center flex-column" style={{ gap: '.5rem' }}>
               <div className="d-flex align-items-center justify-content-center" style={{ gap: '.5rem' }}>
-                <Button>-</Button>
+                <Button onClick={() => decreaseItemQuantity(id)}>-</Button>
                 <div>
                   <span className="fs-3">{quantity}</span>
                   in cart
                 </div>
-                <Button>+</Button>
+                <Button onClick={() => increaseItemQuantity(id)}>+</Button>
               </div>
-              <Button variant="danger" size="sm">Remove</Button>
+              <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>Remove</Button>
             </div>
           }
         </div>
